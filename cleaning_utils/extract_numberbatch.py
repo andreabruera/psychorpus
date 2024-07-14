d815ee304dac47787a7e1e54f46029aa4bfbad30
Andrea <andrea.bruera@gmail.com>
2024-07-14 22:15:56 +0100

    up

--- cleaning_utils/extract_numberbatch.py
+++ cleaning_utils/extract_numberbatch.py
@@ -4,18 +4,22 @@ import pickle
 
 from tqdm import tqdm
 
+lang = 'en'
+base_folder = os.path.join('/', 'import', 'cogsci', 'andrea', 'dataset', 'word_vectors')
+assert os.path.exists(base_folder)
+
 vecs = dict()
 
 with tqdm() as counter:
-    with open('numberbatch-19.08.txt') as i:
+    with open(os.path.join(base_folder, 'numberbatch-19.08.txt')) as i:
         for l in i:
             line = l.strip().split('/')
             try:
-                if line[2] == 'it':
+                if line[2] == lang:
                     w_v = line[3].split()
                     vecs[w_v[0]] = numpy.array(w_v[1:], dtype=numpy.float32)
                     counter.update(1)
             except IndexError:
                 continue
-with open(os.path.join('it', 'conceptnet_it.pkl'), 'wb') as o:
+with open(os.path.join(base_folder, lang, 'conceptnet_{}.pkl'.format(lang)), 'wb') as o:
     pickle.dump(vecs, o)
